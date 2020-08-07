55ffe30a98cd6d83736e008b5184333f677d5b15
jeogv83 <jeogv83@gmail.com>
2020-08-07 15:54:21 -0400

    Clase camión y Gestión de Camiones Práctico 5

--- src/Camion.java
+++ src/Camion.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
-
 public class Camion {
+
+    //atributos
+    private int chapa;
+    private String color;
+
+    //constructor sin parametro
+    public Camion(){
+        this.color = "rojo";
+        this.chapa = 1234;
+    }
+
+    //constructor con un oarametro
+    public Camion (int unaChapa){
+        this.color = "Desconocido";
+        this.chapa = unaChapa;
+    }
+
+    //constructor con 2 parametros
+    public Camion (int unaChapa, String unColor){
+        this.color = unColor;
+        this.chapa = unaChapa;
+    }
+
+    //metodos de acceso y modificacion
+    public int getChapa(){
+        return this.chapa;
+    }
+
+    public void setChapa(int unaChapa) {
+        this.chapa = unaChapa;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String unColor){
+        this.color = unColor;
+    }
+
+    //comparar si dos camiones tienen el mismo color
+    public boolean tieneMismoColor (Camion otroCamion){
+        return this.getColor().equals(otroCamion.getColor());
+    }
+
+    public String toString(){
+        return "Camion de color: " + this.getColor() + " con una chapa: " + this.getChapa();
+    }
 }
--- src/GestionCamiones.java
+++ src/GestionCamiones.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class GestionCamiones {
+
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+
+        Camion c1 = new Camion();
+        c1.setChapa(4444);
+        c1.setColor("Negro");
+        System.out.println(c1);
+        /*System.out.println("La chapa del camion es: " + c1.getChapa());
+        System.out.println("El color del camion es: " + c1.getColor());*/
+
+        Camion c2 = new Camion();
+        System.out.println(c2);
+
+        Camion c3 = new Camion(123);
+        System.out.println(c3);
+
+        Camion c4 = new Camion(9000, "Verde");
+        System.out.println(c4);
+
+        System.out.println("Ingrese el color del camion: ");
+        String colorCamion = input.nextLine();
+        System.out.println("Ingrese la chapa del camion: ");
+        int chapaCamion = input.nextInt();
+
+        Camion c5 = new Camion(chapaCamion, colorCamion);
+        System.out.println(c5);
+
+        boolean sonIguales = c1.tieneMismoColor(c2);
+        if (sonIguales == true){
+            System.out.println("Los camiones tienen el mismo color");
+        }else{
+            System.out.println("Los camiones No tienen el mismo color");
+        }
+        //c2.tieneMismoColor(c3);
+
+    }
+
 }
